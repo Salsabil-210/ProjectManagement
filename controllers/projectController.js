7bf1000e11fdcb17dfa26e8bcbe2a75923e38210
salsabil <sosokhaled528@gmail.com>
2025-06-28 22:03:13 +0300

    CRUD operations for projects

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -75,3 +75,83 @@ exports.getprojects = async (req, res) => {
     }
 };
    
+exports.updateProject = async (req, res) => {
+    if (!req.user || !req.user.isAdmin) {
+        return res.status(403).json({
+            success: false,
+            message: "You are not authorized to update a project"
+        });
+    }
+    try {
+        const { name, description, start_date, end_date, userid } = req.body;
+        const { id } = req.params; 
+
+        const project = await db.query(
+            'SELECT id FROM projects WHERE id = $1',
+            [id]
+        );
+
+        if (project.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "There is no project like this to update"
+            });
+        }
+
+        const updatedProject = await db.query(
+            `UPDATE projects
+             SET name = $1, description = $2, start_date = $3, end_date = $4, user_id = $5
+             WHERE id = $6
+             RETURNING *`,
+            [name, description, start_date, end_date, userid, id]
+        );
+
+        return res.status(200).json({
+            success: true,
+            message: "Project updated successfully",
+            data: updatedProject.rows[0]
+        });
+    } catch (error) {
+        console.error('Error updating the project:', error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        });
+    }
+};
+
+exports.deleteProject = async (req, res) => {
+    if (!req.user || !req.user.isAdmin) {
+        return res.status(403).json({
+            success: false,
+            message: `You are not authorized to delete this project`
+        });
+    }
+
+    try {
+        const { id } = req.params;
+
+        const project = await db.query(
+            `DELETE FROM projects WHERE id = $1 RETURNING id`,
+            [id]
+        );
+
+        if (project.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: `Project not found to delete`
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: `Project deleted successfully`
+        });
+    } catch (error) {
+        console.error(`Error deleting the project:`, error);
+        res.status(500).json({
+            success: false,
+            message: `Server error while deleting the project`
+        });
+    }
+};
