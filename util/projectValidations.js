76fa4b030efb36e461d10eec51d5f40d7b955e7b
salsabil <sosokhaled528@gmail.com>
2025-06-28 22:34:58 +0300

    Validations of projects

--- util/projectValidations.js
+++ util/projectValidations.js
@@ -1,2 +1,13 @@
-const Joi = require("joi")
+const Joi = require("joi");
 
+name: Joi.string()
+    .trim()
+    .min(1)
+    .max(50)
+    .required()
+    .messages({
+        "string.empty": "Name of project is required",
+        "string.min": "Name must be at least 1 character",
+        "string.max": "Name must be less than or equal to 50 characters",
+        "any.required": "Name is required"
+    })
